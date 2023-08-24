c3eeff5980377e4ba7349b89cf7da77313f69e00
julia-015 <136704485+julia-015@users.noreply.github.com>
2023-08-24 08:36:48 -0300

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -9,3 +9,11 @@ for numero_quarto in range(1, 5):
     hotel.adicionar_quarto(ApartamentoDuploCasal(numero_quarto))
     hotel.adicionar_quarto(ApartamentoLuxo(numero_quarto))
     hotel.adicionar_quarto(ApartamentoMaster(numero_quarto))
+
+def main():
+    while True:
+        try:
+            print("+------------------------------+ \n | BEM VINDO(A) AO ENCHANTED ECHO RESORT! | \n +------------------------------+ \n [1] Descrição do Hotel \n [2] Apartamentos disponíveis \n [3] Realizar reserva \n [4] Cancelar reserva \n [5] Serviços disponíveis \n [6] Sair")
+
+        except ValueError:
+            print('Problema: Digito não correspondente/ Opção Indisponível')
