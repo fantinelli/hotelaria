b852146df3858c7fb8c1e0c7f18cfa3a4cb88d8a
julia-015 <136704485+julia-015@users.noreply.github.com>
2023-08-24 08:44:08 -0300

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -13,7 +13,12 @@ for numero_quarto in range(1, 5):
 def main():
     while True:
         try:
-            print("+------------------------------+ \n | BEM VINDO(A) AO ENCHANTED ECHO RESORT! | \n +------------------------------+ \n [1] Descrição do Hotel \n [2] Apartamentos disponíveis \n [3] Realizar reserva \n [4] Cancelar reserva \n [5] Serviços disponíveis \n [6] Sair")
+            print("+------------------------------------------+ \n | BEM VINDO(A) AO ENCHANTED ECHO RESORT! | \n+------------------------------------------+ \n [1] Descrição do Hotel \n [2] Apartamentos disponíveis \n [3] Realizar reserva \n [4] Cancelar reserva \n [5] Serviços disponíveis \n [6] Sair")
+
+            escolha = int(input("> "))
+
+            if escolha == 1:
+                print("Nosso hotel: \n \n Bem-vindo ao Enchanted Echo Resort, onde a magia encontra o conforto. \n Situado em um cenário deslumbrante, nosso resort oferece uma experiência encantadora que combina luxo com natureza. \n Cada detalhe do nosso espaço, desde a arquitetura até os jardins bem cuidados, é projetado para criar uma atmosfera mágica e relaxante. \n Nossos quartos e suítes foram concebidos para serem verdadeiros refúgios, com decoração elegante e conforto incomparável. \n As vistas panorâmicas da natureza exuberante que nos rodeia são um espetáculo à parte. \n O Enchanted Echo Resort é o lugar perfeito para se desconectar do mundo e se conectar com a serenidade da natureza.")
 
         except ValueError:
             print('Problema: Digito não correspondente/ Opção Indisponível')
