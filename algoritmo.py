fb355c2a14dbd9bc09ef92f71e9ddccaf3a8e606
julia-015 <136704485+julia-015@users.noreply.github.com>
2023-08-24 08:26:37 -0300

    Update algoritmo.py

--- algoritmo.py
+++ algoritmo.py
@@ -1,3 +1,11 @@
 from classes import *
 
 hotel = Hotel("Enchanted Echo Resort")
+
+for numero_quarto in range(1, 5):
+    hotel.adicionar_quarto(ApartamentoSimples(numero_quarto))
+    hotel.adicionar_quarto(ApartamentoSimplesCasal(numero_quarto))
+    hotel.adicionar_quarto(ApartamentoDuplo(numero_quarto))
+    hotel.adicionar_quarto(ApartamentoDuploCasal(numero_quarto))
+    hotel.adicionar_quarto(ApartamentoLuxo(numero_quarto))
+    hotel.adicionar_quarto(ApartamentoMaster(numero_quarto))
