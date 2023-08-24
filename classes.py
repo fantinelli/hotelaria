0a11664ffa107c83054e4b8bc40633a8ed0ff80b
yRuanz <134006568+yRuanz@users.noreply.github.com>
2023-08-24 07:46:59 -0300

    Update classes.py

---
+++ classes.py
@@ -0,0 +1,14 @@
+class Hotel:
+
+    class Quartos:
+        class APsimples:
+
+        class APsimplesC:
+
+        class APduplo:
+
+        class APduploC:
+
+        class APluxo:
+            
+        class APmaster:
